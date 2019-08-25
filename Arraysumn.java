08eac48f0f1ecd126c0ccb795d896134a6f1f3b7
Sridhar C <52724943+sridhar98c@users.noreply.github.com>
2019-08-25 13:35:32 +0530

    Create Arraysumn.java

---
+++ Arraysumn.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Arraysumn{
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        int x = myNumbers[1][2];
+        System.out.println(x);
+	}
+}
